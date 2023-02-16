e64c45457d5679fcb3a8a05e6042d34cc676e0ca
Kirill <kiruxin16@gmail.com>
2023-02-16 23:46:19 +0700

    Home work to lesson 3

--- wnteredshop/src/main/java/ru/geekbrains/spring/wnteredshop/WinteredShopApplication.java
+++ wnteredshop/src/main/java/ru/geekbrains/spring/wnteredshop/WinteredShopApplication.java
@@ -2,12 +2,17 @@ package ru.geekbrains.spring.wnteredshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import ru.geekbrains.spring.wnteredshop.entities.Product;
+import ru.geekbrains.spring.wnteredshop.services.ProductService;
 
 @SpringBootApplication
 public class WinteredShopApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WinteredShopApplication.class, args);
+		ConfigurableApplicationContext context= SpringApplication.run(WinteredShopApplication.class, args);
+/*		ProductService productService = context.getBean(ProductService.class);
+		productService.findProductByID(2L);*/
 	}
 
 }
